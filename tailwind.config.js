c2e0e980d33b1e80919ec200635fd6d7a9f4fc48
Vuong Nguyen <kudoyaung@gmail.com>
2025-09-09 13:50:47 +0700

    feat(tailwind): configure tailwind with typography plugin

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+import typography from "@tailwindcss/typography";
+
+const config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+};
+
+export default config;
